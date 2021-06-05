ad5b03d2aa3967c456c38f785ea1aad2f1553c0b
Nathan <maximenathan@hotmail.fr>
2021-06-05 20:21:39 +0200

    Bocker : Back/Front add shared volumes to containers

--- frontend/srcs/src/App.tsx
+++ frontend/srcs/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo bg-yellow-400" alt="logo" />
+        <img src={logo} className="bg-yellow-400 App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
