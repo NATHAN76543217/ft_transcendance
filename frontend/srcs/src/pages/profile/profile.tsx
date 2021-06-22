30ffed5967b4521322c3f0a1804c12d2578fd7cd
bjacob-56 <bricejacob56@gmail.com>
2021-06-22 17:02:36 +0200

    front: React: add Profile Page

--- frontend/srcs/src/pages/profile/profile.tsx
+++ frontend/srcs/src/pages/profile/profile.tsx
@@ -1,10 +1,33 @@
-function Profile()
-{
-    return (
-        <div>
-            profile page
-        </div>
-    );
+import React from 'react';
+
+// import Button from '../../components/utilities/Button';
+import UserInformation from '../../components/userInformation/userInformation';
+import UserStats from '../../components/userStats/userStats';
+import MatchHistory from '../../components/matchHistory/matchHistory';
+
+class Profile extends React.Component {
+    render() {
+        return (
+            <div className="w-full">
+                <section className="relative w-full">
+                    <UserInformation
+                        name="Login"
+                        status="Connected" />
+                </section>
+                <div className="relative flex flex-wrap justify-center w-full">
+                    <section className="relative">
+                        <UserStats
+                        nbWin={15}
+                        nbLoss={5}
+                        />
+                    </section>
+                    <section className="relative">
+                        <MatchHistory />
+                    </section>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Profile;
