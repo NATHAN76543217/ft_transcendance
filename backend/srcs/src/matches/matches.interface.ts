38ea2b436c2e154535f31ddf4831ab2b6f61d646
Your Name <pablolamtenzan@gmail.com>
2021-06-22 15:59:48 +0200

    back: nestjs: Matches: skeleton & basic functions built

---
+++ backend/srcs/src/matches/matches.interface.ts
@@ -0,0 +1,10 @@
+export default interface Match
+{
+    idMatch : number;
+    idPlayerOne : string;
+    idPlayerTwo : string;
+    scorePlayerOne : number;
+    scorePlayerTwo : number;
+    startTime : Date;
+    endTime: Date;
+}
