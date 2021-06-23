1ecd45dd23c8bf458a5cd8a318d3011448c7ea58
Clément Hamada <clementhamada@protonmail.com>
2021-06-23 23:09:55 +0200

    frontend: TextInput: Add error span

--- frontend/srcs/src/components/utilities/TextInput.tsx
+++ frontend/srcs/src/components/utilities/TextInput.tsx
@@ -1,7 +1,7 @@
 // TODO: Hint
 
 //import { ChangeEvent, useState } from "react";
-import { Path, UseFormRegister } from "react-hook-form";
+import { FieldError, Path, UseFormRegister } from "react-hook-form";
 
 type TextInputProps<FormValues> = {
     name: Path<FormValues>;
@@ -10,6 +10,7 @@ type TextInputProps<FormValues> = {
     required: boolean;
     labelClass: string;
     inputClass: string;
+    error?: FieldError;
 }
 
 /* const validateValue = (initialValue: string) => {
@@ -30,15 +31,29 @@ type TextInputProps<FormValues> = {
 
 export {}
 
-export function TextInput<FormValues>({name, register, type, required, labelClass, inputClass}: TextInputProps<FormValues>) {
+export function TextInputError(error?: FieldError) {
+    let message: string = "";
+
+    if (error)
+    {
+        if (error.message)
+            message = error.message;
+        else if (error.type === "required")
+            message = "This field is required";
+    }
+    return (<span>{message}</span>)
+}
+
+export function TextInput<FormValues>({name, register, type, required, labelClass, inputClass, error}: TextInputProps<FormValues>) {
     //const {value, bind, reset} = validateValue("");
 
     //const { register, handleSubmit, watch, formState: { errors } } = useForm();
 
     return (
         <div>
-            <label htmlFor={name} className={`capitalize m-5 border-1 ${labelClass}`}>{name}</label>
+            <label htmlFor={name} className={`capitalize ${labelClass}`}>{name}</label>
             <input type={type} id={name} placeholder={`Enter ${name}`} className={`w-full ${inputClass}`} {...register(name, { required })}/>
+            {TextInputError(error)}
         </div>
     );
 }
--- frontend/srcs/src/pages/login/login.tsx
+++ frontend/srcs/src/pages/login/login.tsx
@@ -11,19 +11,21 @@ export function Login(props: {})
 {
     //const [username, password] = useState();
 
-    const { register, handleSubmit } = useForm<ILoginFormValues>();
+    const { register, handleSubmit, formState: { errors } } = useForm<ILoginFormValues>();
 
-    const onSubmit = (values: ILoginFormValues) => {
-        axios.post("localhost:8080/login", values);
+    const onSubmit = async (values: ILoginFormValues) => {
+        const data = await axios.post("localhost:8080/login", values);
+
+        console.log(data);
     };
 
     return(
-        <section className="w-96 rounded-xl m-auto mt-32 p-4 bg-neutral">
-            <h1 className='mt-2 text-6xl text-center'>Login</h1>
-            <form onSubmit={handleSubmit(onSubmit)} method='POST' className="w-full flex flex-col">
-                <TextInput name="username" register={register}></TextInput>
-                <TextInput name="password" register={register} type="password"></TextInput>
-                <button type='button' className={ "inline-block rounded-xl text-neutral font-semibold p-2 mt-4 text-lg bg-secondary"}>Login</button>
+        <section className="flex flex-col max-w-sm p-4 m-auto mt-32 rounded-xl bg-neutral">
+            <h1 className='m-4 text-6xl text-center'>Login</h1>
+            <form onSubmit={handleSubmit(onSubmit)} method='POST'>
+                <TextInput name="username" register={register} required={true} error={errors.username}></TextInput>
+                <TextInput name="password" register={register} type="password" required={true} error={errors.password}></TextInput>
+                <input type='submit' value="Login" className={"rounded-xl text-neutral font-semibold p-2 mt-4 w-full text-lg bg-secondary"}></input>
             </form>
         </section>
     );
