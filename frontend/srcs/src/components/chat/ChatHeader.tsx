f84ffb0503c9258aa5a8d3ff497e130570e79488
Clément Hamada <clementhamada@protonmail.com>
2021-07-02 05:09:34 +0200

    frontend: chats: Add ChatHeader

--- frontend/srcs/src/components/chat/ChatHeader.tsx
+++ frontend/srcs/src/components/chat/ChatHeader.tsx
@@ -1,14 +1,29 @@
-import { ReactComponent } from "*.svg"
 import { Chat } from "../../models/Chat"
+import { ChatTitle } from "./ChatTitle"
 
 type ChatHeaderProps = {
-    chat: Chat
+    children: JSX.Element | JSX.Element[];
+    chat?: Chat;
 }
 
-export function ChatHeader({}: ChatHeaderProps) {
-    return (
-    <header className="flex w-full h-10 bg-gray-200 justify-evenly">
-       <div>Hello</div>
-       <div>Hello</div>
-    </header>)
+export function ChatHeader({chat, children}: ChatHeaderProps) {
+    if (chat) {
+        return (
+            <header className="flex justify-between w-full h-10 p-2 bg-gray-200">
+                <ChatTitle chat={chat}></ChatTitle>
+                <div className="flex items-center space-x-2">
+                    {children}
+                </div>
+            </header>
+        )
+    }
+    else {
+        return (
+            <header className="w-full h-10 bg-gray-200"></header>
+        )
+    }
+}
+
+ChatHeader.defaultProps = {
+    children: (<div/>)
 }
