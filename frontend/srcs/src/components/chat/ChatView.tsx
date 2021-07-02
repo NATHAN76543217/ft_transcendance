b7b3b9353c13046dcaadd03f2c29cce2cd8d4a6f
Clément Hamada <clementhamada@protonmail.com>
2021-07-02 05:38:13 +0200

    frontend: chats: Move ToolTipIconButton to utils

--- frontend/srcs/src/components/chat/ChatView.tsx
+++ frontend/srcs/src/components/chat/ChatView.tsx
@@ -1,26 +1,6 @@
-import { NavLink } from "react-router-dom";
 import { ChatHeader } from "../../components/chat/ChatHeader";
 import { Chat } from "../../models/Chat";
-
-type TooltipIconButtonProps = {
-    tooltip: string;
-    icon: string;
-    iconStyle: string;
-    href: string;
-}
-
-export function TooltipIconButton({ icon, iconStyle, tooltip, href }: TooltipIconButtonProps) {
-    return (
-        <NavLink className="flex flex-col items-center has-tooltip" activeClassName="text-blue-600" to={href}>
-            <i className={`${iconStyle} ${icon}`}/>
-            <span className="p-1 mt-6 bg-gray-100 rounded shadow-lg tooltip">{tooltip}</span>
-        </NavLink>
-    );
-}
-
-TooltipIconButton.defaultProps = {
-    iconStyle: "fas fa-lg"
-}
+import { TooltipIconButton } from "../utilities/TooltipIconButton";
 
 export function ChatMessageList() {
     return (
