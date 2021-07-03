c4e6aeea26735175cc8d22839702a5aa05ef3c65
Clément Hamada <clementhamada@protonmail.com>
2021-07-03 15:34:37 +0200

    backend: channels: Add socket.io v4 gateway

---
+++ backend/srcs/src/channels/channels.gateway.ts
@@ -0,0 +1,72 @@
+import { ConnectedSocket, MessageBody, OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway } from "@nestjs/websockets";
+import ChannelsService from "./channels.service";
+import { Socket, Server } from 'socket.io';
+import { Logger } from "@nestjs/common";
+import { SocketWithUser } from "src/authentication/socketWithUser.interface";
+
+@WebSocketGateway()
+export class ChannelsGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+	private logger: Logger = new Logger('ChannelsGateway');
+	
+	constructor(
+		private readonly channelsService: ChannelsService
+	) { }
+
+	afterInit(server: Server) {
+		this.logger.debug(`Listening at ${server.path()}`);
+	}
+
+	async handleConnection(socket: SocketWithUser) {
+		this.logger.debug(`Query: ${JSON.stringify(socket.handshake.query)}`);
+		try {
+			socket.user = await this.channelsService.getUserFromSocket(socket);
+		}
+		catch (e) {
+			socket.disconnect(true);
+			return;
+		}
+
+		socket.emit("authenticated");
+
+		socket.on('disconnecting', () => {
+			// TODO: set offline status
+			this.logger.debug(`Client disconnecting with ${socket.rooms.size} joined rooms`);
+
+			socket.rooms.forEach((r) => {
+				socket.to(r).emit("disconnected", {username: socket.user.name});
+			});
+		});
+
+		// TODO: set online status
+
+		// TODO: Maybe join self named channel to receive friend invitations etc...
+		// Join user channels
+		socket.user.channels.forEach((c) => {
+			const channel = c.id.toString();
+
+			socket.join(channel);
+			// TODO: Check which data to send on join
+			socket.to(channel).emit("connected", {username: socket.user.name});
+		});
+		
+		this.logger.debug(`Client connected with ${socket.rooms.size} joined rooms`);
+
+		return { event: "connected" };
+	}
+
+	async handleDisconnect(socket: Socket) {
+		// Check if we have a room list here
+		this.logger.debug(`Client disconnect with ${socket.rooms.size} joined rooms`);
+	}
+
+	@SubscribeMessage('message')
+	async handleMessage(@ConnectedSocket() socket: Socket, @MessageBody() data: string) {
+		const author = await this.channelsService.getUserFromSocket(socket);
+
+		// TODO: Replace getUser with SocketWithUser
+		// TODO: Broadcast messages by room
+		// TODO: Save messages to repository
+
+		this.logger.debug(`${author.name}: ${data}`);
+	}
+}
--- backend/srcs/src/channels/channels.service.ts
+++ backend/srcs/src/channels/channels.service.ts
@@ -1,28 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import Channel from './channel.entity';
 import { CreateChannelDto } from './dto/createChannel.dto';
 import { UpdateChannelDto } from './dto/updateChannel.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Like, Repository } from 'typeorm';
 import ChannelNotFound from './exception/ChannelNotFound.exception';
+import { Socket } from 'socket.io';
+import { AuthenticationService } from 'src/authentication/authentication.service';
+import { WsException } from '@nestjs/websockets';
+import User from 'src/users/user.interface';
+import { string } from '@hapi/joi';
 
 @Injectable()
 export default class ChannelsService {
   constructor(
+    private readonly authenticationService: AuthenticationService,
     @InjectRepository(Channel)
-    private channelsRepository: Repository<Channel>
-  ) { }
+    private channelsRepository: Repository<Channel>,
+  ) {}
+
+  async getUserFromSocket(socket: Socket): Promise<User> {
+    const logger = new Logger();
+    logger.debug(`headers: ${JSON.stringify(socket.handshake.headers)}, query: ${JSON.stringify(socket.handshake.query)}`);
+    const token = socket.handshake.headers.token as string | undefined;
+
+    if (!token)
+      throw new WsException('Missing token.');
+    logger.debug("auth token: " + token);
+
+    if (token === "TODO")
+      return {id: 0, name: "socketUser", password: "", nbWin: 0, nbLoss: 0, stats: 0, imgPath: "", twoFactorAuth: false, status: "online", channels: []};
+
+    const user =
+      await this.authenticationService.getUserFromAuthenticationToken(
+        token,
+      );
+
+    if (!user) {
+      throw new WsException('Invalid token.');
+    }
+    return user;
+  }
 
   async getAllChannels(name: string) {
     if (name === undefined) {
       return await this.channelsRepository.find({ relations: ['users'] });
     }
     return this.channelsRepository.find({
-      where: { 'name': Like('%' + name + '%') }});
+      where: { name: Like('%' + name + '%') },
+    });
   }
 
   async getChannelById(id: number) {
-    const channel = await this.channelsRepository.findOne(id, { relations: ['users'] });
+    const channel = await this.channelsRepository.findOne(id, {
+      relations: ['users'],
+    });
     if (channel) {
       return channel;
     }
@@ -45,7 +77,9 @@ export default class ChannelsService {
 
   async updateChannel(id: number, channel: UpdateChannelDto) {
     await this.channelsRepository.update(id, channel);
-    const updatedChannel = await this.channelsRepository.findOne(id, { relations: ['users'] });
+    const updatedChannel = await this.channelsRepository.findOne(id, {
+      relations: ['users'],
+    });
     if (updatedChannel) {
       return updatedChannel;
     }
