3dfa26faed07aedc7676b22a0d75049672e04695
Your Name <pablolamtenzan@gmail.com>
2021-07-08 04:07:08 +0200

    front: pong: customization seems to be implemented

--- frontend/srcs/src/components/pong/components/customization.tsx
+++ frontend/srcs/src/components/pong/components/customization.tsx
@@ -1,12 +1,28 @@
 import React from "react"
+import {
+    GameMode
+} from "../../../../../../pong/engine/polimorphiclib";
 import ContiniousSlider from "./continuousSlider"
 
 interface ICustomization
 {
     gameConfig : Array<ContiniousSlider>;
+    gameMode : GameMode
 }
 
-export default class Customization extends React.Component
+export const BALL_SPEED : string = "Ball speed";
+export const BALL_COLOR : string = "Ball color";
+export const COURT_COLOR : string = "Court color";
+export const NET_COLOR : string = "Net color";
+export const PLAYER_ONE_WIDTH : string = "Player one width";
+export const PLAYER_ONE_HEIGHT : string = "Player one height";
+export const PLAYER_ONE_COLOR : string = "Player one color";
+export const PLAYER_TWO_WIDTH : string = "Player two width";
+export const PLAYER_TWO_HEIGHT : string = "Player two height";
+export const PLAYER_TWO_COLOR : string = "Player two color";
+export const BOT_LEVEL : string = "Bot level";
+
+export class Customization extends React.Component
 {
     constructor(
         props : Readonly<{}>,
@@ -45,6 +61,25 @@ export default class Customization extends React.Component
                 <div className={this.sliderDivClassName}>
                     {this.data.gameConfig[7 % this.data.gameConfig.length]}
                 </div>
+                {
+                    this.data.gameMode == GameMode.SINGLE_PLAYER
+                    ?
+                    <div className="">
+                        <div className={this.sliderDivClassName}>
+                            {this.data.gameConfig[8 % this.data.gameConfig.length]}
+                        </div>
+                        <div className={this.sliderDivClassName}>
+                            {this.data.gameConfig[9 % this.data.gameConfig.length]}
+                        </div>
+                        <div className={this.sliderDivClassName}>
+                            {this.data.gameConfig[9 % this.data.gameConfig.length]}
+                        </div>
+                        <div className={this.sliderDivClassName}>
+                            {this.data.gameConfig[10 % this.data.gameConfig.length]}
+                        </div>
+                    </div>
+                    : 0
+                }
             </div>
         );
     }
