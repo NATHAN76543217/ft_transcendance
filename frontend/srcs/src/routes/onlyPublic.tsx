934cb4510384677ace4e2bacf424460d68f5078a
Clément Hamada <chamada@student.le-101.fr>
2021-07-09 15:33:54 +0200

    frontend: OnlyPublic: Fix component property

--- frontend/srcs/src/routes/onlyPublic.tsx
+++ frontend/srcs/src/routes/onlyPublic.tsx
@@ -1,27 +1,24 @@
 import { Redirect, Route } from "react-router-dom";
 
-function PrivateRoute(props : {
-    isAuth : boolean,
+function PrivateRoute(props: {
+    isAuth: boolean,
     component?: any,
     exact?: boolean,
     path?: string,
-    children: React.ReactNode | JSX.Element
-    }) : JSX.Element
-    {
-        return (
-            <Route
-                { ...props.exact }
-                { ...props.path }
-                render={ () => {
-                    if (props.isAuth === true)
-                        return <Redirect to="/"/>
-                    else
-                        return (
-                        <div>
-                            { props.children }
-                        </div>);
-                }}
-            />
-        );
-    };
+    children?: React.ReactNode | JSX.Element
+}): JSX.Element {
+    return (
+        <Route
+            exact={props.exact}
+            path={props.path}
+            component={props.component}
+            render={() => {
+                if (props.isAuth === true)
+                    return <Redirect to="/" />
+                else
+                    return <div>props.children</div>;
+            }}
+        />
+    );
+};
 export default PrivateRoute;
