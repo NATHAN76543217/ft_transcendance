d83f61d43085843570e00b303d4c0977e5d3621a
Clement Hamada <clementhamada@protonmail.com>
2021-07-16 19:14:49 +0200

    backend: message: entity: Add channel_id JoinColumn

--- backend/srcs/src/messages/message.entity.ts
+++ backend/srcs/src/messages/message.entity.ts
@@ -1,26 +1,35 @@
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, Timestamp, UpdateDateColumn, ManyToOne } from "typeorm";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
 import Channel from '../channels/channel.entity';
 
 @Entity()
 export default class Message {
-    @PrimaryGeneratedColumn()
-    public id: number;
-    
-    @Column()
-    public sender_id: number;
+  @PrimaryGeneratedColumn()
+  public id: number;
 
-    @Column()
-    public channel_id: number;
+  @Column()
+  public sender_id: number;
 
-    @CreateDateColumn()
-    public created_at: Date;
+  @Column()
+  public channel_id: number;
 
-    @UpdateDateColumn()
-    public updated_at: Date;
+  @CreateDateColumn()
+  public created_at: Date;
 
-    @Column()
-    public text: string;
+  @UpdateDateColumn()
+  public updated_at: Date;
 
-    @ManyToOne(() => Channel, (channel: Channel) => channel.messages)
-    public channel: Channel;
+  @Column()
+  public text: string;
+
+  @ManyToOne(() => Channel, (channel: Channel) => channel.messages)
+  @JoinColumn({ name: 'channel_id', referencedColumnName: 'id' })
+  public channel: Channel;
 }
