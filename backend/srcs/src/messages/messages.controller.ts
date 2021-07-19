16df4d0b1d73b7c4d1ed5fe5f432a9b77bc848f5
Clement Hamada <clementhamada@protonmail.com>
2021-07-19 19:31:27 +0200

    backend: message: controller: Remove unused routes

--- backend/srcs/src/messages/messages.controller.ts
+++ backend/srcs/src/messages/messages.controller.ts
@@ -1,42 +1,48 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Put, UseGuards } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Delete,
+  Param,
+  Patch,
+  UseGuards,
+} from '@nestjs/common';
 
 import MessagesService from './messages.service';
 import CreateMessageDto from './dto/createMessage.dto';
 import UpdateMessageDto from './dto/updateMessage.dto';
-import JwtAuthenticationGuard from "src/authentication/jwt-authentication.guard";
+import JwtAuthenticationGuard from 'src/authentication/jwt-authentication.guard';
 
 // TODO: Message CASL
 @Controller('messages')
 @UseGuards(JwtAuthenticationGuard)
 export default class MessagesController {
-    constructor(
-        private readonly messageService: MessagesService
-    ) { }
+  constructor(private readonly messageService: MessagesService) {}
 
-   /*  @Get()
-    @UseGuards(JwtAuthenticationGuard)
+  /*  @Get()
     getAllMessages() {
         return this.messageService.getAllMessages();
     }
-    */
-    /* 
     @Get(':id')
     getMessageById(@Param('id') id: string) {
         return this.messageService.getMessageById(Number(id));
-    } */
+    }
 
     @Post()
     async createMessage(@Body() message: CreateMessageDto) {
         return this.messageService.createMessage(message);
     }
+    */
+  // TODO: Authorization
+  @Patch(':id')
+  async updateMessage(
+    @Param('id') id: string,
+    @Body() message: UpdateMessageDto,
+  ) {
+    return this.messageService.updateMessage(Number(id), message);
+  }
 
-    @Patch(':id')
-    async updateMessage(@Param('id') id: string, @Body() message: UpdateMessageDto) {
-        return this.messageService.updateMessage(Number(id), message);
-    }
-
-    @Delete(':id')
-    async deleteMessage(@Param('id') id: string) {
-        return this.messageService.deleteMessage(Number(id));
-    }
+  @Delete(':id')
+  async deleteMessage(@Param('id') id: string) {
+    return this.messageService.deleteMessage(Number(id));
+  }
 }
