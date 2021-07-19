81f69ee64d8140434b09539436f9d0e699fd3549
Pablo Lamtenzan <plamtenz@z4r7p5.42lyon.fr>
2021-07-19 17:51:54 +0200

    pong: old implementation erased

--- pong/exceptions/isSamePlayer.exception.ts
+++
@@ -1,9 +0,0 @@
-import {
-    HttpException
-} from '@nestjs/common'
-
-export default class IsSamePlayer extends HttpException
-{
-    constructor(idPlayer : string)
-    { super(`Forbidden: ${idPlayer} vs ${idPlayer}.`, HttpStatus.FORBIDDEN); }
-}
