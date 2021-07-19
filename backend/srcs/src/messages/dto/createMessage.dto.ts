2796eb2937288b71eaf8d6401c389dcd50b07186
Clement Hamada <clementhamada@protonmail.com>
2021-07-19 19:28:59 +0200

    backend: message: dto: Use interface instead of class

--- backend/srcs/src/messages/dto/createMessage.dto.ts
+++ backend/srcs/src/messages/dto/createMessage.dto.ts
@@ -1,6 +1,6 @@
 import { MessageType } from '../message.entity';
 
-export default class CreateMessageDto {
+export default interface CreateMessageDto {
   channel_id: number;
   type: MessageType;
   data: string;
