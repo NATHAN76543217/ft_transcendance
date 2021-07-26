3645f1a73241b5f42c5adbfd929c0b2c25088e54
bjacob-56 <bricejacob56@gmail.com>
2021-07-26 13:45:43 +0200

    back: chat: use old validatora instead of NameValidator for channels

--- backend/srcs/src/channels/dto/createChannel.dto.ts
+++ backend/srcs/src/channels/dto/createChannel.dto.ts
@@ -10,8 +10,16 @@ import {
 import { NameValidator } from 'src/utils/NameValidator';
 import { ChannelMode } from '../utils/channelModeTypes';
 
+const fieldName = 'Username'
+
 export class CreateChannelDto {
-  @NameValidator('Channel name')
+  // @NameValidator('Channel name')
+  @IsString({ message: `${fieldName} is invalid!` })
+  @IsNotEmpty({ message: `${fieldName} cannot be empty!` })
+  @Matches('^([0-9a-z\\-\\_])+$', undefined, {
+    message: `${fieldName} is invalid!`
+  })
+  @Length(1, 15)
   name: string;
 
   @IsString()
--- backend/srcs/src/channels/dto/updateChannel.dto.ts
+++ backend/srcs/src/channels/dto/updateChannel.dto.ts
@@ -4,13 +4,22 @@ import {
   IsNumber,
   Length,
   Matches,
+  IsNotEmpty,
 } from 'class-validator';
 import { NameValidator } from 'src/utils/NameValidator';
 import { ChannelMode } from '../utils/channelModeTypes';
 
+const fieldName = 'Username'
+
 export class UpdateChannelDto {
   @IsOptional()
-  @NameValidator('Channel name')
+  // @NameValidator('Channel name')
+  @IsString({ message: `${fieldName} is invalid!` })
+  @IsNotEmpty({ message: `${fieldName} cannot be empty!` })
+  @Matches('^([0-9a-z\\-\\_])+$', undefined, {
+    message: `${fieldName} is invalid!`
+  })
+  @Length(1, 15)
   name: string;
 
   @IsOptional()
