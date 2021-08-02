faccb6c49167d9038a497d968e2e3755427e4542
bjacob-56 <bricejacob56@gmail.com>
2021-08-02 10:44:22 +0200

    front: game: add loading screen on matchmaking

--- frontend/srcs/src/components/loading/loading.tsx
+++ frontend/srcs/src/components/loading/loading.tsx
@@ -1,10 +1,14 @@
-function Loading() {
+type LoadingProps = {
+  hideText?: boolean
+} 
+
+function Loading(props: LoadingProps) {
   return (
-    <div className="z-50 flex flex-col items-center w-full h-auto mt-36 ">
+    <div className="z-50 flex flex-col items-center w-full h-auto ">
       <i className="mb-2 text-secondary-dark fas fa-circle-notch fa-spin fa-3x"></i>
-      <h2 className="text-xl font-semibold text-center text-secondary-dark">
+      {props.hideText ? <div/> : <h2 className="text-xl font-semibold text-center text-secondary-dark">
         Loading...
-      </h2>
+      </h2>}
     </div>
   );
 }
