3ec4e1f4b3971be549da3d895d46d37289e67d17
bjacob-56 <bricejacob56@gmail.com>
2021-08-08 10:12:50 +0200

    front: reimport cancelGameInvitation which had been erased

--- frontend/srcs/src/pages/game/pages/pong.tsx
+++ frontend/srcs/src/pages/game/pages/pong.tsx
@@ -31,7 +31,7 @@ enum Received {
 }
 
 export function Pong({ match }: RouteComponentProps<PongPageParams>) {
-  const { user, eventSocket: appSocket } = useContext(AppContext);
+  const { user, eventSocket: appSocket, relationshipsList } = useContext(AppContext);
   const { matchSocket } = useContext(AppContext);
   const history = useHistory();
 
@@ -275,7 +275,7 @@ export function Pong({ match }: RouteComponentProps<PongPageParams>) {
 
   useEffect(() => {
     const setPlayerNamesData = async () => {
-      if (!finalData.playersName[0] || !finalData.playersName[0].length) {
+      if (finalData.playersId[0] && finalData.playersId[1] && (!finalData.playersName[0] || !finalData.playersName[0].length)) {
         try {
           const data0 = await axios.get("/api/users/" + finalData.playersId[0]);
           const data1 = await axios.get("/api/users/" + finalData.playersId[1]);
@@ -290,6 +290,26 @@ export function Pong({ match }: RouteComponentProps<PongPageParams>) {
     };
     setPlayerNamesData();
   }, [finalData]);
+
+  const cancelGameRequest = async () => {
+    const relation = relationshipsList.find((relation) => {
+      if (relation.gameInvite) {
+        return relation.gameInvite.sender_id === user?.id
+      } else {
+        return false;
+      }
+    })
+    if (relation) {
+      try {
+        await axios.delete(`/api/matches/${relation.gameInvite?.data}`);
+        await axios.delete(`/api/messages/${relation.gameInvite?.id}`);
+        history.push(`/`);
+      } catch (e) {
+        history.push(`/`);
+        console.error(e);
+      }
+    }
+  };
 
   const quitGameAsSpectator = () => {
     setGiveUpDisplay(false);
@@ -311,11 +331,21 @@ export function Pong({ match }: RouteComponentProps<PongPageParams>) {
         <button
           className={buttonClassname + " bg-red-600 hover:bg-red-700"}
           onClick={quitGameAsSpectator}
-          // disabled={!giveUpDisplay}
+        // disabled={!giveUpDisplay}
         >
           <span className={textButtonClassname}>Quit</span>
         </button>
-      );
+      )
+    } else if (waitingScreen) {
+      return (
+        <button
+          className={buttonClassname + " bg-red-600 hover:bg-red-700"}
+          onClick={cancelGameRequest}
+          disabled={giveUpDisplay}
+        >
+          <span className={textButtonClassname}>Cancel invitation</span>
+        </button>
+      )
     } else if (!giveUpDisplay) {
       return (
         <button
@@ -325,9 +355,9 @@ export function Pong({ match }: RouteComponentProps<PongPageParams>) {
         >
           <span className={textButtonClassname}>Give up</span>
         </button>
-      );
+      )
     }
-  };
+  }
 
   const displayGiveUpConfirmationButton = () => {
     if (giveUpDisplay) {
